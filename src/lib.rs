e3073f60d141d41a8e2a33346462c389256a65cd
rosefarts <gonesbird@gmail.com>
2024-06-03 13:26:51 +0200

    reflect + docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use std::marker::PhantomData;
 
 use bevy_ecs::system::Resource;
+use bevy_reflect::Reflect;
 
-#[derive(Resource, Debug, Clone, Copy)]
+/// A Marker [`Resource`] for "something" that has been initialized.
+///
+/// Usefull if you need to register a system at runtime but want to ensure that it only happens once (since there is no way to know if the system has already been added).
+#[derive(Resource, Reflect, Debug, Clone, Copy)]
 pub struct Initialized<M>(PhantomData<M>);
 
 impl<M> Default for Initialized<M> {
