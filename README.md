4d42c2f1ef487385d1b8c431367bb2c87326286e
rosefarts <gonesbird@gmail.com>
2024-06-03 13:29:01 +0200

    add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+A Marker `Resource` for "something" that has been initialized.
+
+Usefull if you need to add a system at runtime but want to ensure that it only happens once (since there is no way to know if the system has already been added).
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use bevy_reflect::Reflect;
 
 /// A Marker [`Resource`] for "something" that has been initialized.
 ///
-/// Usefull if you need to register a system at runtime but want to ensure that it only happens once (since there is no way to know if the system has already been added).
+/// Usefull if you need to add a system at runtime but want to ensure that it only happens once (since there is no way to know if the system has already been added).
 #[derive(Resource, Reflect, Debug, Clone, Copy)]
 pub struct Initialized<M>(PhantomData<M>);
 
